920705c238ac936db9294cc39b1c81f320a44261
mikko-o <mikko.ojala@mail.com>
2022-11-21 18:04:55 +0200

    add the widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
+import { useWeb3React, Web3ReactProvider } from '@web3-react/core';
+import { PangolinProvider, SwapWidget } from '@pangolindex/components';
 
 function App() {
+  const { library, chainId, account } = useWeb3React()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PangolinProvider library={library} chainId={chainId} account={account ?? undefined}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <SwapWidget />
+        </header>
+      </div>
+    </PangolinProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ethers } from 'ethers';
+import { Web3ReactProvider } from '@web3-react/core';
+
+function getLibrary(provider: any, connector: any) {
+  return new ethers.providers.Web3Provider(provider)
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>
 );
 
